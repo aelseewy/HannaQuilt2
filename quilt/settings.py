727aeb273bce46a64de432078eeb4ab976fa548c
aelseewy <74848911+aelseewy@users.noreply.github.com>
2021-03-06 17:22:15 +0200

    configure whitenoise and db

--- quilt/settings.py
+++ quilt/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%#fhtgfhy$xlvde1vxi2@26k=@ph7$i&$8%)54stj*xijq49nt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 LOGIN_URL = 'login'
@@ -57,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'quilt.urls'
@@ -84,12 +86,17 @@ WSGI_APPLICATION = 'quilt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'quilt_db',
+#        'USER': 'postgres',
+#        'PASSWORD': 'Salamat',
+#        'HOST': 'localhost',
+#    }
+#}
+
+DATABASES = {'default': dj_database_url.config(default='postgres://postgres:Salamat@localhost/quilt_db')}
 
 
 # Password validation
@@ -153,3 +160,6 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'quiltinegypt@gmail.com'
 EMAIL_HOST_PASSWORD = 'zzcucrhusiwnufhw'
 EMAIL_USE_TLS = True
+
+# Whitenoise settings
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
